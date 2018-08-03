853337b7ad367ea7a0b816772c9c742757a3d1f8
Taiviv <T@ivivi250780>
2018-08-03 11:01:31 +0200

    Change Constants.java

--- app/src/main/java/com/chartier/virginie/moodtracker/utils/Constants.java
+++ app/src/main/java/com/chartier/virginie/moodtracker/utils/Constants.java
@@ -2,13 +2,19 @@ package com.chartier.virginie.moodtracker.utils;
 
 import com.chartier.virginie.moodtracker.R;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 /**
  * Created by Virginie Chartier alias Taiviv on 28/06/2018.
  */
+
+// This class collects the constants of general utility
 public abstract class Constants {
 
     private Constants(){}
 
+    public static DateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
     public static final int BUNDLE_REQUEST_CODE = 42;
     public static final int[][] LIST_COLOR_IMG = {
             {R.color.faded_red,
