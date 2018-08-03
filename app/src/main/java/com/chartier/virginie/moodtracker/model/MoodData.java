9f2d17d07c9481b38340a11326703a09e90cc8cd
Taiviv <T@ivivi250780>
2018-08-03 11:09:57 +0200

    Change MooData.java

--- app/src/main/java/com/chartier/virginie/moodtracker/model/MoodData.java
+++ app/src/main/java/com/chartier/virginie/moodtracker/model/MoodData.java
@@ -3,12 +3,15 @@ package com.chartier.virginie.moodtracker.model;
 import android.content.Context;
 import android.util.Log;
 
+import com.chartier.virginie.moodtracker.helpers.DateHelper;
 import com.chartier.virginie.moodtracker.utils.Constants;
 
+import java.text.DateFormat;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-
+import java.util.List;
 
 
 /**
@@ -26,6 +29,9 @@ public class MoodData {
      * on enregistre une humeur , la date du jour et le commentaire (notre objet Mood aura toutes ces variables paramétrées)
      * si aucune humeur existe pour le jour d'aujourd'hui alors j'enregistre normalement mon humeur
      * si une humeur existe pour le jour d'aujourd'hui alors j'enregistre la nouvelle humeur
+     *
+     * --Humeur par default--
+     * si  l'utilisateur n'a pas enregistré une humeur alors une humeur par default sera enregistré
      */
 
 
@@ -38,6 +44,7 @@ public class MoodData {
             date = Constants.formatter.parse(dateString);
         } catch (ParseException e) {
             e.printStackTrace();
+            Log.e("TAG", "Mince, une erreur s'est produite avec le formattage de la date :", e); // TODO : Modification de Philippe
         }
         Log.e("TAG", "l'humeur pour la date "+ dateString +" contient (ou pas) le message "+comment+" et l'id "+moodId);
         if (!savedMoods.isEmpty()) {
@@ -49,5 +56,25 @@ public class MoodData {
         }
         savedMoods.add(new Mood(moodId, date, comment));
         MoodPreferences.saveData(context, savedMoods);
+    }
+
+
+    // This method save a default mood if the user has not registered a mood
+    public static List<Mood> weekListMood(Context context) {
+        ArrayList<Mood> weekList = new ArrayList<>();
+        ArrayList<Mood> savedMoods = MoodPreferences.loadData(context);
+        for(int i=7; i >= 0; i--) {
+            boolean found = false;
+            for(Mood mood : savedMoods) {
+                if (DateHelper.getDateCalendar(mood.getmDate()).getTime().equals(DateHelper.getDateCalendarWithoutDays(i).getTime())) {
+                    weekList.add(mood);
+                    found = true;
+                }
+            }
+            if(!found) {
+                weekList.add(new Mood(2, DateHelper.getDateCalendarWithoutDays(i).getTime(), ""));
+            }
+        }
+        return weekList;
     }
 }
