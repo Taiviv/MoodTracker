8f635629f22eebe21bb0aa966fdb53e11398b6e4
Taiviv <T@ivivi250780>
2018-08-05 21:02:40 +0200

    Change Constants.java

--- app/src/main/java/com/chartier/virginie/moodtracker/utils/Constants.java
+++ app/src/main/java/com/chartier/virginie/moodtracker/utils/Constants.java
@@ -27,5 +27,5 @@ public abstract class Constants {
                     R.drawable.smiley_normal,
                     R.drawable.smiley_happy,
                     R.drawable.smiley_super_happy}};
-    public static final int NUMBER_ITEM = 8;
+    public static final int NUMBER_ITEM = 7;
 }
